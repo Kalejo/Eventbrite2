0ae941fe9bf2e6c02757107cb238ca0ae541c1b9
Alexandro <alejo_alvarez67@hotmail.com>
2018-05-08 18:12:38 +0200

    controller users avec view show

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
                :path_names => {:sign_in => 'login', :sign_out => 'logout', :edit => 'profile'}
   root 'home#index'
 
-
+resources :users, only: [:show]
 
 
 end
