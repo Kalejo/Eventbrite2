8a62e35552c9e4075d5032d0aa76f91035164c19
Alexandro <alejo_alvarez67@hotmail.com>
2018-05-09 13:03:30 +0200

    modifier controller photo

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,7 +2,7 @@ class EventsController < ApplicationController
 
 before_action :set_event, only: [:show, :edit, :update]
 before_action :authenticate_user!, except: [:show]
-
+before_action :require_same_user, only: [:edit, :update]
 
 	def index
 		@event = current_user.event
@@ -17,21 +17,37 @@ before_action :authenticate_user!, except: [:show]
     def create
     	@event = current_user.events(event_params)
     		if @event.save
-    			redirect_to @event, notice:"Votre événement a été ajouté avec succès"
+    			if params[:images]
+                    params[:images].each do |i|
+                        @event.photos.create(image: i)
+                    end
+                end
+                @photos = @event.photos
+                redirect_to edit_event_path(@event), notice:"Votre événement a été ajouté avec succès"
     		else
     			render :new
     		end
     end
 
 	def show
-	end
+        @photos = @event.photos
+
+    end
 
 	def edit
+        @photos = @event.photos
+
     end
 
 	def update
     	if @event.update(event_params)
-    		redirect_to @event, notice:"Modification enregistrée..."
+            if params[:images]
+                    params[:images].each do |i|
+                        @event.photos.create(image: i)
+                    end
+                end
+            @photos = @event.photos
+            redirect_to edit_event_path(@event), notice:"Modification enregistrée..."
     	else
     		render :edit
     	end
@@ -54,5 +70,12 @@ before_action :authenticate_user!, except: [:show]
 	def event_params
 		params.require(:event).permit(:description, :date, :place, :price)
 	end
+
+    def require_same_user
+        if current_user.id != @event.user_id
+            flash[:danger] = "Vous n'avez pas le droit de modifier cette page"
+            redirect_to root_path
+        end
+    end
 
 end
